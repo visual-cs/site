56de77675f3d79783159e4a868aa3ddfa6aa728b
Inle Bush <inlebush@gmail.com>
2020-05-16 18:10:13 +0200

    test

--- src/App.js
+++ src/App.js
@@ -10,6 +10,7 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
+        <p>Butts and Poop</p>
         <a
           className="App-link"
           href="https://reactjs.org"
