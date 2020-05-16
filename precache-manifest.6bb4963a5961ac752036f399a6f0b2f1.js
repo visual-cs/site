ec6e1c98f4930b181173111c90d863c8ea5fddcc
Komron <komron_aripov@cdl.ch>
2020-05-16 17:32:19 +0200

    Updates

---
+++ precache-manifest.6bb4963a5961ac752036f399a6f0b2f1.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "572ad8d7405eaa91433b16d8c6003344",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5eea005d2ae999b9b225",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "b97dd454f1c44c9c078b",
+    "url": "/static/js/2.53bc217e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.53bc217e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5eea005d2ae999b9b225",
+    "url": "/static/js/main.38c39915.chunk.js"
+  },
+  {
+    "revision": "7b41a341eda0a090ec3e",
+    "url": "/static/js/runtime-main.0996e8ac.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
