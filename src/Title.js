117bb84e883678784334727cb6fe3395ea171c84
Komron <komron_aripov@cdl.ch>
2020-05-29 23:43:51 +0200

    Title component

---
+++ src/Title.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Title() {
+    return (
+        <div>
+            <h1 className="title">&lt;Visual CS&gt;</h1>
+        </div>
+    )
+}
+
+export default Title;
