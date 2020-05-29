c3d5789b03e4e06dbe441afd74405a01a14d3e6b
Komron <komron_aripov@cdl.ch>
2020-05-29 23:43:38 +0200

    add switch

--- src/App.js
+++ src/App.js
@@ -2,32 +2,39 @@ import React from 'react';
 import { 
   BrowserRouter as Router,
   Switch,
-  Route,
-  Link
+  Route
 } from 'react-router-dom';
 
 import NavBar from './NavBar';
 import About from './About';
 import Home from './Home';
 import Visual from './Vis';
+import Title from './Title';
+import './App.scss';
 
 
 function App() {
   return (
     <Router>
 
+      <Title />
+      
       <NavBar />
 
-      <Route exact path="/">
-        <Home />
-      </Route>
-      <Route exact path="/about_us">
-        <About />
-      </Route>
-      <Route exact path="/visualizations">
-        <Visual />
-      </Route>
-  
+      <div className="section">
+        <Switch>
+          <Route exact path="/">
+            <Home />
+          </Route>
+          <Route exact path="/about_us">
+            <About />
+          </Route>
+          <Route exact path="/visualizations">
+            <Visual />
+          </Route>
+        </Switch>
+      </div>
+
     </Router>
   );
 }
