0394de33c0761d48698ae9e6d95505a3cd671899
Komron <komron_aripov@cdl.ch>
2020-05-30 18:29:28 +0200

    add title-hero

--- src/Title.js
+++ src/Title.js
@@ -2,8 +2,10 @@ import React from 'react';
 
 function Title() {
     return (
-        <div>
-            <h1 className="title">&lt;Visual CS&gt;</h1>
+        <div className="hero is-small has-background-grey-lighter">
+            <div className="hero-body">
+                <h1 className="title">&lt;Visual CS&gt;</h1>
+            </div>
         </div>
     )
 }
