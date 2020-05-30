fce9c01bd9ebc449274d3849dc0532cabf79ff37
Komron <komron_aripov@cdl.ch>
2020-05-30 18:49:53 +0200

    demo-page

--- src/App.js
+++ src/App.js
@@ -21,19 +21,17 @@ function App() {
       
       <NavBar />
 
-      <div className="section">
-        <Switch>
-          <Route exact path="/">
-            <Home />
-          </Route>
-          <Route exact path="/about_us">
-            <About />
-          </Route>
-          <Route exact path="/visualizations">
-            <Visual />
-          </Route>
-        </Switch>
-      </div>
+      <Switch>
+        <Route exact path="/">
+          <Home />
+        </Route>
+        <Route exact path="/about_us">
+          <About />
+        </Route>
+        <Route exact path="/visualizations">
+          <Visual />
+        </Route>
+      </Switch>
 
     </Router>
   );
--- src/Home.js
+++ src/Home.js
@@ -3,9 +3,43 @@ import React from 'react';
 function Home() {
     return (
         <div>
-            <h1>
-                Home Page
-            </h1>
+            <div className="section has-background-white-ter">
+                <h1 className="title">About</h1>
+                <p>
+                But I must explain to you how all this mistaken 
+                idea of denouncing of a pleasure and praising 
+                pain was born and I will give you a complete 
+                account of the system, and expound the actual 
+                teachings of the great explorer of the truth, 
+                the master-builder of human happiness: dat ass.
+                </p>
+            </div>
+            <div className="section has-background-white-ter">
+                <h1 className="title">Progress</h1>
+                <p>
+                To take a trivial example, which of us ever 
+                undertakes laborious physical exercise, except to 
+                obtain some advantage from it? But who has any right 
+                to find fault with a man who chooses to enjoy a 
+                pleasure that has no annoying consequences, or one 
+                who avoids a pain that produces no resultant 
+                pleasure? On the other hand, we denounce with 
+                righteous indignation and dislike men who are so 
+                beguiled and demoralized by the charms of pleasure of 
+                the moment, so blinded by desire, that they cannot 
+                foresee the pain and trouble that are bound to 
+                ensue; and equal blame belongs to those who fail 
+                in their duty through weakness of will, which is the 
+                same as saying through shrinking from toil and pain. 
+                These cases are perfectly simple and easy to 
+                distinguish. In a free hour, when our power of 
+                choice is untrammeled and when nothing prevents our 
+                being able to do what we like best, every pleasure is 
+                to be welcomed and every pain avoided. But in certain 
+                circumstances and owing to the claims of duty or 
+                the obligations.
+                </p>
+            </div>
         </div>
     );
 }
