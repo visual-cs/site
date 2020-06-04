cce26be9d5d0a4cb1a9f914cae4889b43f559beb
Inle Bush <inlebush@gmail.com>
2020-06-04 20:47:44 +0200

    about

--- src/About.js
+++ src/About.js
@@ -3,48 +3,66 @@ import React from 'react';
 function About() {
   return (
       <div>
-          <div className="section has-background-white-ter">
-            <h1 className="title is-1">Meet Us:</h1>
-          </div>
-          <div className="column is-one-half">
-            <div className="box has-background-white has-text-left">
-                <h1 className="title is-2">Inle Bush</h1>
-                <p>
-                But I must explain to you how all this mistaken 
-                idea of denouncing of a pleasure and praising 
-                pain was born and I will give you a complete 
-                account of the system, and expound the actual 
-                teachings of the great explorer of the truth, 
-                the master-builder of human happiness: dat ass.
-                </p>
-            </div>
-            <div className="box has-background-white">
-                <h1 className="title is-2">Komron Aripov</h1>
-                <p>
-                To take a trivial example, which of us ever 
-                undertakes laborious physical exercise, except to 
-                obtain some advantage from it? But who has any right 
-                to find fault with a man who chooses to enjoy a 
-                pleasure that has no annoying consequences, or one 
-                who avoids a pain that produces no resultant 
-                pleasure? On the other hand, we denounce with 
-                righteous indignation and dislike men who are so 
-                beguiled and demoralized by the charms of pleasure of 
-                the moment, so blinded by desire, that they cannot 
-                foresee the pain and trouble that are bound to 
-                ensue; and equal blame belongs to those who fail 
-                in their duty through weakness of will, which is the 
-                same as saying through shrinking from toil and pain. 
-                These cases are perfectly simple and easy to 
-                distinguish. In a free hour, when our power of 
-                choice is untrammeled and when nothing prevents our 
-                being able to do what we like best, every pleasure is 
-                to be welcomed and every pain avoided. But in certain 
-                circumstances and owing to the claims of duty or 
-                the obligations.
-                </p>
+        <div className="section has-background-white-ter">
+          <h1 className="title is-1 has-text-centered">Meet Us:</h1>
+        </div>
+        <div className="columns has-background-white-ter">
+          <div className="column has-background-white-ter is-5 is-offset-1">
+            <div class="card">
+              <div class="card-image">
+                <figure class="image">
+                  <img src="https://bulma.io/images/placeholders/1280x960.png" alt="Placeholder"></img>
+                </figure>
+              </div>
+              <div class="card-content">
+                <div class="media">
+                  <div class="media-left">
+                    <figure class="image is-48x48">
+                      <img src="https://bulma.io/images/placeholders/96x96.png" alt="Placeholder"></img>
+                    </figure>
+                  </div>
+                  <div class="media-content">
+                    <p class="title is-4">Inle Bush</p>
+                    <p class="subtitle is-6">@imbush</p>
+                  </div>
+                </div>
+
+                <div class="content">
+                  Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                  Phasellus nec iaculis mauris.
+                </div>
+              </div>
             </div>
           </div>
+
+          <div className="column has-background-white-ter is-5">
+            <div class="card">
+              <div class="card-image">
+                <figure class="image is-">
+                  <img src="https://bulma.io/images/placeholders/1280x960.png" alt="Placeholder"></img>
+                </figure>
+              </div>
+              <div class="card-content">
+                <div class="media">
+                  <div class="media-left">
+                    <figure class="image is-48x48">
+                      <img src="https://bulma.io/images/placeholders/96x96.png" alt="Placeholder"></img>
+                    </figure>
+                  </div>
+                  <div class="media-content">
+                    <p class="title is-4">Komron Aripov</p>
+                    <p class="subtitle is-6">@karipov</p>
+                  </div>
+                </div>
+
+                <div class="content">
+                  Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                  Phasellus nec iaculis mauris.
+                </div>
+              </div>
+            </div>
+          </div>    
+        </div>
       </div>
   );
 }
