816943102cd847ae6b5032c71086e20a3c31a194
imbush <inlebush@gmail.com>
2020-06-22 21:14:46 +0200

    added footer

---
+++ src/common/Footer.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+function Footer() {
+    return (
+        <div className="footer has-background-grey-lighter">
+            <div className="content">
+                <p>
+                <strong>Visual-CS</strong> created by <a href="https://github.com/imbush">Inle Bush</a> and <a href="https://github.com/karipov">Komron Aripov</a>.<br></br> 
+                The source code can be found <a href="https://github.com/visual-cs/visual-cs.github.io">here</a>.
+                </p>
+            </div>
+        </div>
+    )
+}
+
+export default Footer;
