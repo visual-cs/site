f019b54de10d6872d6b5791c0237a7c2b448750b
Komron <komron_aripov@cdl.ch>
2020-06-24 20:20:16 +0200

    MIT license footer

--- src/common/Footer.js
+++ src/common/Footer.js
@@ -6,7 +6,7 @@ function Footer() {
       <div className="content">
         <p>
         <strong>Visual-CS</strong> created by <a href="https://github.com/imbush">Inle Bush</a> and <a href="https://github.com/karipov">Komron Aripov</a>.<br></br> 
-        The source code can be found <a href="https://github.com/visual-cs/visual-cs.github.io">here</a>.
+        Source code is released under the MIT license and available publicly on <a href="https://github.com/visual-cs/visual-cs.github.io">GitHub</a>.
         </p>
       </div>
     </div>
