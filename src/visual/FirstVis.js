c01d7f6710cd5aec14682c8bf57a0349026d78cf
imbush <inlebush@gmail.com>
2020-06-25 12:17:15 +0200

    example_vis setup

--- src/visual/FirstVis.js
+++ src/visual/FirstVis.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import P5Wrapper from 'react-p5-wrapper';
 
-function sketch (p) {
-    let rotation = 0;
-  
+function cardioid (p) {
+    var x = 0;
+
     p.setup = function () {
         p.createCanvas(600, 400);
     };
@@ -15,7 +15,9 @@ function sketch (p) {
     // };
   
     p.draw = function () {
-        p.circle(200, 200, 30);
+        p.background(0, 0, 0, 50);
+        p.circle(300, 200 + 100 * p.sin(x), 30);
+        x += 0.1;
     };
 
     p.mouseClicked = function () {
@@ -25,6 +27,83 @@ function sketch (p) {
 
 export default function FirstVis() {
     return(
-        <P5Wrapper sketch={sketch} />
+        <div>
+            <div className="section has-background-white-ter">
+                <p className="subtitle is-10">
+                    As the inaugural post on our new website, I would like 
+                    to begin by saying “Hello World!” and expressing my 
+                    hope that you enjoy the site. It was an adventure to 
+                    build and we had a lot of fun doing so. :)
+                </p>    
+            </div>
+            
+            <div className="section has-background-white-ter">
+                <h1 className="title">Epicycloids by Inle Bush</h1>
+                <p>
+                    An epicycloid is the curve traced by following a point on 
+                    the circumference of one circle rolling around another. 
+                    It is demonstrated simply in the following visualization:
+                </p>
+            </div>
+
+            <div className="section has-background-white-ter">
+                [// gif of rolling circle]
+            </div>
+
+            <div className="section has-background-white-ter">
+                <p>
+                    If the outer circle has radius r and the inner circle has 
+                    radius R, we can define the ratio between these two sides, 
+                    R/r = k. Varying k results in some interesting patterns.
+                </p>
+            </div>
+            
+            <div className="section has-background-white-ter">
+                <P5Wrapper sketch={cardioid} />
+            </div>
+            
+            <div className="section has-background-white-ter">
+                <p>
+                    For integer k, the number of “leaves” on the epicyclod is equal 
+                    to k. For rational k, p/q as an irreducible fraction, then k has 
+                    p “leaves”. Setting k to an irrational number, like pi, causes a 
+                    non repeating path which eventually fills the entire annulus (donut) 
+                    with inner radius R and outer radius R + 2r.
+                </p>
+            </div>
+            
+            <div className="section has-background-white-ter">
+                [// image epicycloid where k=pi]
+            </div>
+
+            <div className="section has-background-white-ter">
+                <p>
+                    Interestingly, for any epicycloid with integer k, the 
+                    epicycloid will have cusps (inner points) at all fractions 
+                    of a full rotation which are multiples of 1/k. For example, 
+                    when k = 3, cusps are present at 1/3, 2/3 and 3/3 of a full 
+                    rotation around the circle. This is a result of the epicycloid 
+                    splitting the rotation into k “leaves”. 
+                    By plotting all of the epicycloids where 0 {'<'} k {'<='} n, 
+                    I am able to create a visual representation of all of the 
+                    fractions {'<'} 1, which can be represented as an integral fraction 
+                    with denominator {'<='} n. In other words, this program displays all 
+                    rational numbers, p/q, where q {'<='} n.
+                </p>
+
+            </div>
+            
+            <div className="section has-background-white-ter">
+                <P5Wrapper sketch={cardioid} />
+            </div>
+
+            <div className="section has-background-white-ter">
+                <p>
+                    If n is increased to infinity, the angle placements of each of the 
+                    cusps in the image will represent every rational numbers. I find 
+                    this pretty cool.
+                </p>
+            </div>
+        </div>
     );
 }
