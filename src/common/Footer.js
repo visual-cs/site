b6d0cf1d4186783edd0a35b2e3a3a3eb5bf367de
Komron <komron_aripov@cdl.ch>
2020-06-25 21:22:40 +0200

    simplified footer

--- src/common/Footer.js
+++ src/common/Footer.js
@@ -3,9 +3,9 @@ import React from 'react';
 function Footer() {
   return (
     <div className="footer has-background-grey-lighter">
-      <div className="content">
+      <div className="content has-text-centered">
         <p>
-        <strong>Visual-CS</strong> created by <a href="https://github.com/imbush">Inle Bush</a> and <a href="https://github.com/karipov">Komron Aripov</a>.<br></br> 
+        ⓒ 2020 — <a href="https://github.com/imbush">Inle Bush</a> and <a href="https://github.com/karipov">Komron Aripov</a>.<br></br> 
         Source code is released under the MIT license and available publicly on <a href="https://github.com/visual-cs/visual-cs.github.io">GitHub</a>.
         </p>
       </div>
