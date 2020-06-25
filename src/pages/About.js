fdec3f9a1c535c93128957bec0838ae662860fd1
Komron <komron_aripov@cdl.ch>
2020-06-25 21:22:29 +0200

    about cards mostly done

--- src/pages/About.js
+++ src/pages/About.js
@@ -3,25 +3,20 @@ import React from 'react';
 function About() {
   return (
       <div>
-        <div className="section has-background-white-ter">
+        <div className="section">
           <h1 className="title is-1 has-text-centered">Meet Us:</h1>
         </div>
-        <div className="section has-background-white-ter">
-          <div className="columns has-background-white-ter">
-            <div className="column has-background-white-ter is-5 is-offset-1">
+        <div className="section">
+          <div className="columns">
+            <div className="column is-5 is-offset-1">
               <div class="card">
                 <div class="card-image">
                   <figure class="image">
-                    <img src="https://bulma.io/images/placeholders/1280x960.png" alt="Placeholder"></img>
+                    <img src="images/komron.png" alt="komron"></img>
                   </figure>
                 </div>
                 <div class="card-content">
                   <div class="media">
-                    <div class="media-left">
-                      <figure class="image is-48x48">
-                        <img src="https://bulma.io/images/placeholders/96x96.png" alt="Placeholder"></img>
-                      </figure>
-                    </div>
                     <div class="media-content">
                       <p class="title is-4">Komron Aripov</p>
                       <p class="subtitle is-6"><a href="https://github.com/karipov">@karipov</a></p>
@@ -29,27 +24,21 @@ function About() {
                   </div>
 
                   <div class="content">
-                    Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-                    Phasellus nec iaculis mauris.
+                    Math enthusiast and aspiring software developer.
                   </div>
                 </div>
               </div>
             </div>
 
-            <div className="column has-background-white-ter is-5">
+            <div className="column is-5">
               <div class="card">
                 <div class="card-image">
                   <figure class="image is-">
-                    <img src="https://bulma.io/images/placeholders/1280x960.png" alt="Placeholder"></img>
+                    <img src="images/inle.png" alt="inle"></img>
                   </figure>
                 </div>
                 <div class="card-content">
                   <div class="media">
-                    <div class="media-left">
-                      <figure class="image is-48x48">
-                        <img src="https://bulma.io/images/placeholders/96x96.png" alt="Placeholder"></img>
-                      </figure>
-                    </div>
                     <div class="media-content">
                       <p class="title is-4">Inle Bush</p>
                       <p class="subtitle is-6"><a href="https://github.com/imbush">@imbush</a></p>
