e70cdeb91d3db30ff59a42546290160f9514b57b
Komron <komron_aripov@cdl.ch>
2020-06-26 09:51:20 +0200

    sectioned navbar

--- src/common/NavBar.js
+++ src/common/NavBar.js
@@ -3,16 +3,19 @@ import { Link } from "react-router-dom";
 
 function NavBar() {
   return (
-    <nav className="navbar is-dark" role="navigation" aria-label="main navigation">
-      <div className="container has-text-centered ">
-      <div className="navbar-menu">
-        <div className="navbar-start">
-          <Link className="navbar-item is-expanded has-text-weight-semibold is-size-5" to="/">/home</Link>
-          <Link id="center-item" className="navbar-item is-expanded has-text-weight-semibold is-size-5" to="/visual">/visual</Link>
-          <Link className="navbar-item is-expanded has-text-weight-semibold is-size-5" to="/about">/about</Link>
+    <div>
+      <nav className="navbar is-dark" role="navigation" aria-label="main navigation">
+        <div className="container has-text-centered ">
+          <div className="navbar-menu">
+            <div className="navbar-start">
+              <Link className="navbar-item is-expanded has-text-weight-semibold is-size-5" to="/">/home</Link>
+              <Link id="center-item" className="navbar-item is-expanded has-text-weight-semibold is-size-5" to="/visual">/visual</Link>
+              <Link className="navbar-item is-expanded has-text-weight-semibold is-size-5" to="/about">/about</Link>
+            </div>
+          </div>
         </div>
-      </div></div>
-    </nav>
+      </nav>
+    </div>
   );
 }
 
