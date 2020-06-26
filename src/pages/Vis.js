b6717ae067fe0d3a4001077a2a60e43df06349be
Komron <komron_aripov@cdl.ch>
2020-06-26 19:39:34 +0200

    removed placeholders

--- src/pages/Vis.js
+++ src/pages/Vis.js
@@ -5,7 +5,6 @@ function Visual() {
   return (
     <div className="section">
       <div class="tile is-ancestor">
-
         <div class="tile is-parent">
           <article class="tile is-child box">
             <p class="title">Tenprint</p>
@@ -25,49 +24,7 @@ function Visual() {
             </figure>
           </article>
         </Link>
-
-        <div class="tile is-parent">
-          <article class="tile is-child box">
-            <p class="title">Hello World</p>
-            <p class="subtitle">What is up?</p>
-            <figure class="image is-square">
-              <img src="https://bulma.io/images/placeholders/640x480.png"></img>
-            </figure>
-          </article>
-        </div>
       </div>
-
-      <div class="tile is-ancestor">
-        <div class="tile is-parent">
-          <article class="tile is-child box">
-            <p class="title">Hello World</p>
-            <p class="subtitle">What is up?</p>
-            <figure class="image is-4by3">
-              <img src="https://bulma.io/images/placeholders/640x480.png"></img>
-            </figure>
-          </article>
-        </div>
-        <div class="tile is-parent">
-          <article class="tile is-child box">
-            <p class="title">Hello World</p>
-            <p class="subtitle">What is up?</p>
-            <figure class="image is-4by3">
-              <img src="https://bulma.io/images/placeholders/640x480.png"></img>
-            </figure>
-          </article>
-        </div>
-        <div class="tile is-parent">
-          <article class="tile is-child box">
-            <p class="title">Hello World</p>
-            <p class="subtitle">What is up?</p>
-            <figure class="image is-4by3">
-              <img src="https://bulma.io/images/placeholders/640x480.png"></img>
-            </figure>
-          </article>
-        </div>
-      </div>
-
-      
     </div>
   );
 }
