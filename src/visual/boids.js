09bdcab4341e20946420f029843761b9138dea52
karipov <karipov@protonmail.com>
2020-08-13 01:56:42 +0200

    boids writing++ and code spacing fixed

--- src/visual/boids.js
+++ src/visual/boids.js
@@ -1,5 +1,22 @@
 import React from 'react';
+import P5Wrapper from 'react-p5-wrapper';
 import Highlight from 'react-highlight';
+
+
+function boids(p) {
+    let canvas;
+
+    p.setup = function () {
+        let style = window.getComputedStyle(document.getElementById('P5Canvas1'), ':after');
+        let canvas_width = parseFloat(style.width);
+        let canvas_height = parseFloat(style.height);
+        console.log(canvas_width, canvas_height);
+
+        canvas = p.createCanvas(canvas_width, canvas_height);
+        canvas.parent('P5Canvas1');
+        p.background(100);
+    }
+}
 
 export default function boids_html() {
     return(
@@ -36,44 +53,89 @@ export default function boids_html() {
 
                     <h2>Code</h2>
                     <p>
-                        Firstly, we must define a number of constants:
+                        For this project we use <strong>p5.js</strong>. Firstly, we must define a number of constants:
+                    </p>
+                    <Highlight className="javascript">{[
+                        'let flock;',
 
-                        <Highlight className="javascript">{[
-                            'let flock;',
-
-                            'let max_velocity = 2;',
-                            'let max_force = 0.5;',
-                            'let flock_size = 200;'
-                        ].join('\n')}</Highlight>
-
+                        'let max_velocity = 2;',
+                        'let max_force = 0.5;',
+                        'let flock_size = 200;'
+                    ].join('\n')}</Highlight>
+                    <p>
                         These will be used later in the program to help us define how many birds
                         we want, at what speed they should move and how suddenly should they change
                         their movement. The <strong>flock</strong> variable is declared undefined;
                         it will be later used to store the boids themselves.
                     </p>
-                    
+                    <p>
+                        In the setup function for p5.js we initialize the canvas and the flock in the following manner:
+                    </p>
+
+                    <Highlight className="javascript">{[
+                        'function setup() {',
+                        '   createCanvas(500, 500);',
+                        '',
+                        '   flock = [];',
+                        '   for (let i = 0; i < flock_size; i++) {',
+                        '       flock.push(new Boid(',
+                        '           position = createVector(random(width), random(height)),',
+                        '           velocity = createVector(random(-10, 10), random(-10, 10)),',
+                        '           acceleration = createVector(random(-4, 4), random(-4, 4))',
+                        '       ));',
+                        '   }',
+                        '}',
+                    ].join('\n')}</Highlight>
+
+                    <p>
+                        As you can see here we are defining that <strong>flock</strong> variable as
+                        a list and pushing into it a newly initialized <strong>Boid</strong> class.
+                        This class is what will be responsible for applying each of the above rules
+                        to every boid. Within the initialization we are passing p5.js vectors
+                        related to <em>position</em>, <em>velocity</em> and <em>acceleration</em>.
+                        These vectors are randomly generated to a certain range.
+                    </p>
+
+                    <Highlight className="javascript">{[
+                        'class Boid {',
+                        '    constructor(',
+                        '        position,',
+                        '        velocity,',
+                        '        acceleration,',
+                        '        sight',
+                        '    ) {',
+                        '        this.position = position;',
+                        '        this.velocity = velocity;',
+                        '        this.acceleration = acceleration;',
+                        '        this.sight = sight;',
+                        '    }',
+                        '',
+                        '    updatePosition() {}',
+                        '',
+                        '    show() {}',
+                        '}'
+                    ].join('\n')}</Highlight>
+
                     <p>
                         The actual implementation of the program is best done in parts: first
                         implementing cohesion, then alignment, and then separation.
                     </p>
+
                     <h4>Cohesion:</h4>
-                </div>
 
+                    <h2>Demo</h2>
 
-                <div className="content">
                     <h2>Sources</h2>
-                    <p>
-                        <ol type="1">
-                            <li><a href="https://www.youtube.com/watch?v=mhjuuHl6qHM">
-                                Coding Challenge #124: Flocking Simulation
-                            </a></li>
-                            <li><a href="http://www.red3d.com/cwr/boids/">
-                                Boids - Background and Update, by Craig Reynolds</a></li>
-                            <li><a href="http://www.vergenet.net/~conrad/boids/pseudocode.html">
-                                Boids Pseudocode by Conrad Parker © 1995-2010
-                            </a></li>
-                        </ol>
-                    </p>
+                    <ol type="1">
+                        <li><a href="https://www.youtube.com/watch?v=mhjuuHl6qHM">
+                            Coding Challenge #124: Flocking Simulation
+                        </a></li>
+                        <li><a href="http://www.red3d.com/cwr/boids/">
+                            Boids - Background and Update, by Craig Reynolds</a></li>
+                        <li><a href="http://www.vergenet.net/~conrad/boids/pseudocode.html">
+                            Boids Pseudocode by Conrad Parker © 1995-2010
+                        </a></li>
+                    </ol>
                 </div>
 
 
